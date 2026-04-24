f095c1b5307c954c90cf2835093cbc1096b971d6
ThiagoMaluf <thiagomalufhof@gmail.com>
2026-04-24 08:55:24 -0300

    Implementação da validação de entrada e seleção de perfil de usuário.

--- main.c
+++ main.c
@@ -23,4 +23,16 @@ int main() {
     char confirma;
 
     printf("--- ChargeGrid Intelligence | Terminal de Recarga ---\n");
+
+    do {
+        printf("\nSelecione o Tipo de Usuario:\n");
+        printf("1 - Usuario Comum (Tarifa Standard)\n");
+        printf("2 - Usuario Premium (15%% de Desconto)\n");
+        printf("Escolha: ");
+        scanf("%d", &tipoUsuario);
+ 
+        if (tipoUsuario < 1 || tipoUsuario > 2) {
+            printf("[ERRO] Opcao invalida. Tente novamente.\n");
+        }
+    } while (tipoUsuario < 1 || tipoUsuario > 2);
 }
